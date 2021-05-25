17736fb9ee09ac23bbfb4ba6b61ae3091635142b
sokunbidk <daudasokunbi@gmail.com>
2021-05-25 11:32:11 +0100

    my test project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHub.gitIgnore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! .");
+        }
+    }
+}
